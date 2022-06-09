7886d6e519e6ca0efb284c8c770b1182448d9618
eunhxa <42601724+eunhxa@users.noreply.github.com>
2022-06-09 18:40:45 +0900

    nodemon 추가 및 gitignore를 이용한 비밀정보 보호 구현

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+const config = require('./config/key')
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://eunhwa:HtGfgwQ27LT5pq2a@hellocluster.sb3u7.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true
 }).then(() => console.log('MongoDB connected!'))
 .catch(err => console.log(err));
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!~ 안녕하세요')
+  res.send('Hello World!')
+})
+
+const { User } = require("./models/User")
+
+app.post('/register', (req, res) => {
+    // 회원가입 시 필요한 정보를 client에서 가져와서 데이터베이스에 넣기
+    const user = new User(req.body);
+    user.save((err, userInfo) => {
+        if(err) return res.json({success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
 })
 
 app.listen(port, () => {
